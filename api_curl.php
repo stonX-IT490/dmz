ec140f61bbd56cd9de4863bf4fb4c86241996af4
Pokeb <pb438@njit.edu>
2022-03-17 15:41:15 -0400

    First Api test commit

---
+++ api_curl.php
@@ -0,0 +1,17 @@
+$queryString = http_build_query([
+    'api_token' => 'ISPtzsyQxX9QtBmj54SFI8zhFRRuIwXlFyIvZyGR',
+    'symbols' => 'AAPL,MSFT, AMZN',
+    'filter_entities' => 'true',
+    'limit' => 50,
+]);
+
+$ch = curl_init(sprintf('%s?%s', 'https://api.stockdata.org/v1/news/all', $queryString));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$json = curl_exec($ch);
+
+curl_close($ch);
+
+$apiResult = json_decode($json, true);
+
+print_r($apiResult);
