032869811f81e28fad2693164f86f7cc42fb7a42
ag2244 <ag2244@njit.edu>
2022-03-19 22:28:41 -0400

    Now saves only the important data in a condensed format

--- getnews.php
+++ getnews.php
@@ -1,59 +1,73 @@
 #!/usr/bin/php
 <?php
-
-$newslimit = 2;
 $outfile = date("Y-m-d")."_news.json";
+$keys = json_decode(file_get_contents("keys.json"));
 
-function addNewsToFile($stocks)
+function getKey($index)
 {
-	global $newslimit;
+	global $keys;
+	return $keys[$index % count($keys)];
+}
+
+function addNewsToFile($stock, $key)
+{
 	global $outfile;
 	
 	$queryString = http_build_query([
-		'api_token' => file_get_contents("keys.json"),
-		'symbols' => implode(',', $stocks),
+		'api_token' => $key,
+		'symbols' => $stock,
 		'filter_entities' => 'true',
-		'limit' => $newslimit,
+		'limit' => 2,
+		'language' => 'en'
 	]);
 
 	$ch = curl_init(sprintf('%s?%s', 'https://api.stockdata.org/v1/news/all', $queryString));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	$json = curl_exec($ch);
-	$apiResult = json_decode($json, true);
+	$apiResult = json_decode(curl_exec($ch), true)["data"];
 
 	curl_close($ch);
+	
+	$json = "\"".$stock."\":[";
+	
+	foreach($apiResult as $i => $article)
+	{
+		if ($i != 0) $json .= ", ";
+		
+		$json .= json_encode(
+			array(
+				'link' => $article["url"],
+				'title' => $article["title"],
+				'description' => $article["description"],
+				'snippet' => $article["snippet"],
+				'image' => $article["image_url"],
+				'date' => $article["published_at"]
+			)
+		);
+	}
+	
+	$json .= "]";
 
 	file_put_contents($outfile, $json, FILE_APPEND);
 }
 
 function generateAllDailyNews($stocks)
 {
-	global $newslimit;
 	global $outfile;
 	
 	$c = count($stocks);
 	
-	file_put_contents($outfile, "[");
+	file_put_contents($outfile, "{");
 	
-	for ($i = 0; $i < $c; $i += $newslimit)
+	for ($i = 0; $i < $c; $i ++)
 	{
 		if ($i != 0) file_put_contents($outfile, ",", FILE_APPEND);
 		
-		$slice = array_slice($stocks, $i, $i + min($newslimit, $c - $i));
-		
-		addNewsToFile($slice);
-		
-		/*
-		var_dump($i);
-		printf("/");
-		var_dump($i + min(3, $c - $i));
-		printf("\n");
-		*/
+		addNewsToFile($stocks[$i], getKey($i));
 		
 	}
 	
-	file_put_contents($outfile, "]", FILE_APPEND);
+	file_put_contents($outfile, "}", FILE_APPEND);
 	
 	return $c;
 	
