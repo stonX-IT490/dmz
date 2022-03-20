9b221a07a5c7d4e9f7e114c58870303a9afddbb9
ag2244 <ag2244@njit.edu>
2022-03-19 21:18:34 -0400

    Output is now in a valid json format

--- getnews.php
+++ getnews.php
@@ -1,13 +1,19 @@
 #!/usr/bin/php
 <?php
 
+$newslimit = 2;
+$outfile = date("Y-m-d")."_news.json";
+
 function addNewsToFile($stocks)
 {
+	global $newslimit;
+	global $outfile;
+	
 	$queryString = http_build_query([
 		'api_token' => file_get_contents("keys.json"),
 		'symbols' => implode(',', $stocks),
 		'filter_entities' => 'true',
-		'limit' => 2,
+		'limit' => $newslimit,
 	]);
 
 	$ch = curl_init(sprintf('%s?%s', 'https://api.stockdata.org/v1/news/all', $queryString));
@@ -18,17 +24,23 @@ function addNewsToFile($stocks)
 
 	curl_close($ch);
 
-	file_put_contents(date("Y-m-d")."_news.json", $json, FILE_APPEND);
+	file_put_contents($outfile, $json, FILE_APPEND);
 }
 
 function generateAllDailyNews($stocks)
 {
+	global $newslimit;
+	global $outfile;
 	
 	$c = count($stocks);
 	
-	for ($i = 0; $i < $c; $i += 3)
+	file_put_contents($outfile, "[");
+	
+	for ($i = 0; $i < $c; $i += $newslimit)
 	{
-		$slice = array_slice($stocks, $i, $i + min(3, $c - $i));
+		if ($i != 0) file_put_contents($outfile, ",", FILE_APPEND);
+		
+		$slice = array_slice($stocks, $i, $i + min($newslimit, $c - $i));
 		
 		addNewsToFile($slice);
 		
@@ -40,6 +52,8 @@ function generateAllDailyNews($stocks)
 		*/
 		
 	}
+	
+	file_put_contents($outfile, "]", FILE_APPEND);
 	
 	return $c;
 	
