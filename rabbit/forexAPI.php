0c7131f7b1efd4316b6129755ec16f75020fc2c6
ag2244 <ag2244@njit.edu>
2022-05-08 19:06:23 -0400

    Fixed rabbitmq functionality to be easier for the database to handle

--- rabbit/forexAPI.php
+++ rabbit/forexAPI.php
@@ -1,72 +1,58 @@
 #!/usr/bin/php 
  
 <?php 
+//require_once __DIR__ . "/rabbitmq-dmzHost/rabbitMQLib.php";
 
-require_once __DIR__."/rabbitmq-dmzHost/rabbitMQLib.php";
-
-function getForexData() 
-{ 
-	
-		$currencies = [
-			"AUD",
-			"BGN",
-			"BRL",
-			"CAD",
-			"CHF",
-			"CNY",
-			"CZK",
-			"DKK",
-			"EUR",
-			"GBP",
-			"HKD",
-			"HRK",
-			"HUF",
-			"IDR",
-			"ILS",
-			"INR",
-			"ISK",
-			"JPY",
-			"KRW",
-			"MXN",
-			"MYR",
-			"NOK",
-			"NZD",
-			"PHP",
-			"PLN",
-			"RON",
-			"SEK",
-			"SGD",
-			"THB",
-			"TRY",
-			"USD",
-			"ZAR"
-		];
-         
-        $apiURL = "https://www.currency-api.com/rates?base="; 
-		
-		$forexArr = array();
-		$forexArr["currencies"] = $currencies;
-		$forexArr["rates"] = array();
-	
-		for ($i = 0; $i < count($currencies); $i++)
-		{
-			
-			$ch = curl_init();  
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-			curl_setopt($ch, CURLOPT_URL, $apiURL.$currencies[$i]); 
-
-			$apiresult = json_decode(curl_exec($ch), true);
-			
-			array_push($forexArr["rates"], array_values($apiresult["rates"])); 
-			
-		} 
  
-        return $forexArr; 
+function getForexData($currencies) 
+{ 
+         
+	$apiURL = "https://www.currency-api.com/rates?base="; 
+
+	$forexArr = array();
+	$forexArr["currencies"] = $currencies;
+	$forexArr["rates"] = array();
+
+	for ($i = 0; $i < count($currencies); $i++)
+	{
+
+		$ch = curl_init();  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		curl_setopt($ch, CURLOPT_URL, $apiURL.$currencies[$i]); 
+
+		$apiresult = json_decode(curl_exec($ch), true);
+
+		//array_push($forexArr["rates"], array_values($apiresult["rates"])); 
+
+		foreach ($apiresult["rates"] as $exchangeCurr => $rate)
+
+			array_push($forexArr["rates"], 
+				array(
+					"source" => $currencies[$i],
+					"destination" => $exchangeCurr,
+					"rate" => $rate
+			)); 
+
+	} 
+
+	return $forexArr; 
  
 } 
 
 $client = new rabbitMQProducer('amq.direct', 'dmz');
-$client->publish([ 'type' => 'insertForex', 'data' => getForexData()]);
-die();
+$response = $client->send_request([ 'type' => 'getAllCurrencies' ]);
+
+if(!$response) 
+{
+	die("Error comm. with RMQ!\n");
+} 
+
+else if (isset($response['error']) && $response['error']) 
+{
+	die($response['msg']."\n");
+}
+
+$client->publish(["type" => "insertForex", "data" => getForexData($response)]);
 
 ?>
+
