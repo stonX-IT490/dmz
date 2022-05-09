233d25102dd53c5bc421afb538756a5250d873f5
Sarabveer Singh <me@sarabveer.me>
2022-05-08 22:43:07 -0400

    refactor: cleanup curreny api script

--- rabbit/forexAPI.php
+++ rabbit/forexAPI.php
@@ -1,58 +1,47 @@
 #!/usr/bin/php 
- 
 <?php 
-//require_once __DIR__ . "/rabbitmq-dmzHost/rabbitMQLib.php";
-
+require_once __DIR__ . "/rabbitmq-dmzHost/rabbitMQLib.php";
  
-function getForexData($currencies) 
-{ 
-         
-	$apiURL = "https://www.currency-api.com/rates?base="; 
+function getForexData($currencies) {     
+  $apiURL = "https://www.currency-api.com/rates?base="; 
 
-	$forexArr = array();
-	$forexArr["currencies"] = $currencies;
-	$forexArr["rates"] = array();
+  $forexArr = [];
 
-	for ($i = 0; $i < count($currencies); $i++)
-	{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		$ch = curl_init();  
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-		curl_setopt($ch, CURLOPT_URL, $apiURL.$currencies[$i]); 
+  for ($i = 0; $i < count($currencies); $i++) {
+    curl_setopt($ch, CURLOPT_URL, $apiURL.$currencies[$i]); 
+    $apiresult = json_decode(curl_exec($ch), true);
 
-		$apiresult = json_decode(curl_exec($ch), true);
+    foreach ($apiresult["rates"] as $exchangeCurr => $rate) {
+      array_push($forexArr, [
+        "source" => $currencies[$i],
+        "destination" => $exchangeCurr,
+        "rate" => $rate
+      ]);
+    }
+  } 
 
-		//array_push($forexArr["rates"], array_values($apiresult["rates"])); 
-
-		foreach ($apiresult["rates"] as $exchangeCurr => $rate)
-
-			array_push($forexArr["rates"], 
-				array(
-					"source" => $currencies[$i],
-					"destination" => $exchangeCurr,
-					"rate" => $rate
-			)); 
-
-	} 
-
-	return $forexArr; 
- 
+  return $forexArr; 
 } 
 
 $client = new rabbitMQProducer('amq.direct', 'dmz');
 $response = $client->send_request([ 'type' => 'getAllCurrencies' ]);
 
-if(!$response) 
-{
-	die("Error comm. with RMQ!\n");
-} 
-
-else if (isset($response['error']) && $response['error']) 
-{
-	die($response['msg']."\n");
+if(!$response) {
+  die("Error comm. with RMQ!\n");
+} else if (isset($response['error']) && $response['error'])  {
+  die($response['msg']."\n");
 }
 
-$client->publish(["type" => "insertForex", "data" => getForexData($response)]);
+$response = $client->send_request(["type" => "insertForex", "data" => getForexData($response)]);
+
+if(!$response) {
+  die("Error comm. with RMQ!\n");
+} else if (isset($response['error']) && $response['error'])  {
+  die($response['msg']."\n");
+}
 
 ?>
 
